af9d2a564ba65eb3380db1d5d61aaec1051ad1ed
Ben Jackson <ben@adaptivelab.com>
2014-01-27 19:45:27 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsMarketSprintSkeleton::Application.config.secret_key_base = '2f8459312bb55326de61ac1b7f24ebb7783c82a55f4c74c1d0e06a5ad2e4c9a7212398e21139993a269b11b413b63bb8dde09e913463b5a780336f2b51597eeb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsMarketSprintSkeleton::Application.config.session_store :cookie_store, key: '_rails_market_sprint_skeleton_session'
