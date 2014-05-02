af02377733df010be36d2c7b6eb077984ccae78a
Ben Jackson <ben@adaptivelab.com>
2014-05-02 11:23:07 +0100

    Upgraded to Rails 4.1.0 from Rails 4.0.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-RailsMarketSprintSkeleton::Application.routes.draw do
+Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root "home#index"
